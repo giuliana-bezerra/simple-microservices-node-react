7c77f566a92d6202d08ac1d2df09b8ec0aafd800
Giuliana Silva Bezerra <giu.drawer@gmail.com>
2021-02-17 17:29:36 -0300

    add list and create comment operations

---
+++ blog-comments/src/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+
+const app = express();
+app.use(bodyParser.json());
+
+let commentsByPostId = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post('/posts/:id/comments', (req, res) => {
+  const id = randomBytes(4).toString('hex');
+  const { content } = req.body;
+  const comments = commentsByPostId[req.params.id] || [];
+  comments.push({ id, content });
+  commentsByPostId[req.params.id] = comments;
+  res.status(201).send(comments);
+});
+
+app.listen(4001, () => {
+  console.log('Listening on 4001...');
+});
